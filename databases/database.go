9f309fdb4fc2626d2e18e371465a6aba31ad98e8
RINO Heinrich <rino@ohmycorp.com>
2024-02-07 12:50:02 +0300

    add Middlewares, helpers,database and controllers

--- databases/database.go
+++ databases/database.go
@@ -11,22 +11,18 @@ import (
 )
 
 func DBSet() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://rishav_4waytechnologies:<Password>@cluster0.0aeeqhe.mongodb.net/?retryWrites=true&w=majority"))
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017/"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = client.Ping(context.TODO(), nil)
+	err = client.Ping(ctx, nil)
 	if err != nil {
 		log.Println("Failed to Connect")
 		return nil
 	}
-	fmt.Println("Successfully Connected to the Mongodb")
+	fmt.Println("Successfully Connected to the MongoDB")
 	return client
 }
 
